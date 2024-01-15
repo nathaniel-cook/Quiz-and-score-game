08e68348cbae00eb302f6160acd2c7e5d3676582
elryi <nathaniel.edward.cook@gmail.com>
2024-01-15 13:55:43 -0500

    added object for questions/answers

---
+++ script.js
@@ -0,0 +1,35 @@
+const questionList = [
+    {
+        question: "What is 1+1?",
+        answers: {
+            a: "1",
+            b: "2",
+            c: "3",
+            d: "4"
+        },
+        rightAnswer: "d"
+    },
+    {
+        question: "What is the capital of Kentucky?",
+        answers: {
+            a: "La Grange",
+            b: "Louisville",
+            c: "Lexington",
+            d: "Frankfort"
+        },
+        rightAnswer: "d"
+    },
+    {
+        question: "Which one of these is a fruit?",
+        answers: {
+            a: "Apple",
+            b: "Carrot",
+            c: "Potato",
+            d: "Pig"
+        },
+        rightAnswer: "a"
+    }
+]
+// console.log(questionList[0].answers.c) // 3
+// console.log(questionList[1]) // "what is the capital of Kentucky"
+// console.log(questionList[2].rightAnswer) // a
